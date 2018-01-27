91f8a76cd9eaee0d5ce82710063a193ad8de0d90
aib628 <531305146@qq.com>
2018-01-27 23:00:25 +0800

    当前位置寻找优化

--- src/cc/vmaster/finder/impl/MyPositionFinder.java
+++ src/cc/vmaster/finder/impl/MyPositionFinder.java
@@ -6,11 +6,19 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
 import cc.vmaster.Phone;
 import cc.vmaster.finder.TimeRecodFinder;
+import cc.vmaster.finder.helper.MapHelper;
+import cc.vmaster.finder.helper.PixelContainer;
+import cc.vmaster.finder.helper.XLineAscComparator;
+import cc.vmaster.finder.helper.YLineAscComparator;
 import cc.vmaster.helper.CoordinateChecker;
 import cc.vmaster.helper.IOUtils;
 import cc.vmaster.helper.ImageHelper;
@@ -36,28 +44,26 @@ public class MyPositionFinder extends TimeRecodFinder {
 	public int[] find(BufferedImage image, int[] beginPoint, int[] endPoint) {
 		clearDebug();
 
-		int maxX = minInt;// 初始化X最大值
-		int minX = maxInt;// 初始化X最小值
-		int maxY = minInt;// 初始化Y最大值
-		int minY = maxInt;// 初始化Y最小值
-
 		CoordinateChecker.checkAdjustPoints(image, beginPoint, endPoint);
 		for (int x = beginPoint[0]; x < endPoint[0]; x++) {
 			for (int y = beginPoint[1]; y < endPoint[1]; y++) {
 				int pixel = image.getRGB(x, y);
 				RGB rgb = RGB.calcRGB(pixel);
 				if (this.matched(rgb, RGB_TARGET_BOTTLE, 16)) {
-					maxX = Math.max(maxX, x);
-					minX = Math.min(minX, x);
-					maxY = Math.max(maxY, y);
-					minY = Math.min(minY, y);
-
 					if (debug()) {
 						points.add(new int[] { x, y });
 					}
 				}
 			}
 		}
+
+		removeImpossible();
+		Collections.sort(points, XLineAscComparator.instance);
+		int minX = points.get(0)[0];
+		int maxX = points.get(points.size() - 1)[0];
+
+		Collections.sort(points, YLineAscComparator.instance);
+		int maxY = points.get(points.size() - 1)[1];
 
 		int[] result = new int[2];
 		result[0] = (maxX + minX) / 2 + 3;
@@ -66,9 +72,75 @@ public class MyPositionFinder extends TimeRecodFinder {
 		return result;
 	}
 
-	@Override
-	protected void clearDebug() {
+	private void removeImpossible() {
+		removeXLineDiscontinuousPoints();
+		removeYLineDiscontinuousPoints();
+	}
+
+	/**
+	 * 按X轴间隔大于一定值的进行分组，然后保留点数多的集合。并将结果同步回points中
+	 */
+	private void removeXLineDiscontinuousPoints() {
+		Map<Integer, PixelContainer> map = new HashMap<Integer, PixelContainer>();
+		Collections.sort(points, XLineAscComparator.instance);
+
+		int[] point0 = points.get(0);
+		Iterator<int[]> iterator = points.iterator();
+		while (iterator.hasNext()) {
+			int[] point = iterator.next();
+			if (point == point0 || point[0] - point0[0] < 80) {
+				point0 = point;
+				if (map.size() == 0) {
+					map.put(1, new PixelContainer(point));
+				} else {
+					map.get(map.size()).addCount(point);
+				}
+			} else {
+				point0 = point;
+				map.put(map.size() + 1, new PixelContainer(point));
+			}
+		}
+
+		Map<Integer, PixelContainer> sortedMap = MapHelper.reorderCountMap(map);
+		MapHelper.removeUseless(sortedMap, 0, 1);
+
 		points.clear();
+		for (PixelContainer pixel : sortedMap.values()) {
+			points.addAll(pixel.pointList);
+		}
+	}
+
+	/**
+	 * 按Y轴将间隔大于定值的点进行分组，然后移除点数少，保留最大那项
+	 */
+	private void removeYLineDiscontinuousPoints() {
+		Map<Integer, PixelContainer> map = new HashMap<Integer, PixelContainer>();
+		Collections.sort(points, YLineAscComparator.instance);
+
+		int[] point0 = points.get(0);
+		Iterator<int[]> iterator = points.iterator();
+		while (iterator.hasNext()) {
+			int[] point = iterator.next();
+			if (point == point0 || point[1] - point0[1] < 80) {
+				point0 = point;
+				if (map.size() == 0) {
+					map.put(1, new PixelContainer(point));
+				} else {
+					map.get(map.size()).addCount(point);
+				}
+			} else {
+				point0 = point;
+				map.put(map.size() + 1, new PixelContainer(point));
+			}
+		}
+
+		Map<Integer, PixelContainer> sortedMap = MapHelper.reorderCountMap(map);
+		MapHelper.removeUseless(sortedMap, 0, 1);
+
+		points.clear();
+		for (PixelContainer pixel : sortedMap.values()) {
+			points.addAll(pixel.pointList);
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
